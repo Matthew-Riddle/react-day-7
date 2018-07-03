bf049cba8e69be03250bff6c0df2e4d2a472b038
Will Marttala <wmarttala123@gmail.com>
2018-07-03 11:03:54 -0500

    Day 7 notes

--- src/containers/TodoCard/TodoCard.js
+++ src/containers/TodoCard/TodoCard.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import Card from '@material-ui/core/Card'
 import axios from '../../axios-instance'
+import { connect } from 'react-redux'
+import * as actionTypes from '../../store/actions/action-types'
 
 import TodoInput from '../../components/TodoInput/TodoInput'
 import TodoList from '../../components/TodoList/TodoList'
@@ -16,8 +18,7 @@ const reorder = (list, startIndex, endIndex) => {
 
 class TodoCard extends Component {
   state = {
-    value: '',
-    todos: []
+    value: ''
   }
 
   inputChangeHandler = e => {
@@ -34,30 +35,30 @@ class TodoCard extends Component {
 
   addTodo = content => {
     const todos = [
-      ...this.state.todos,
-      { id: this.state.todos.length, content, completed: false }
+      ...this.props.todos,
+      { id: this.props.todos.length, content, completed: false }
     ]
     axios.put('/todos.json', todos).then(() => {
       this.setState(prevState => ({
-        value: '',
-        todos
+        value: ''
       }))
+      this.props.createTodo(todos)
     })
   }
 
   toggleTodo = id => {
-    const todos = this.state.todos.map(
+    const todos = this.props.todos.map(
       todo => (todo.id === id ? { ...todo, completed: !todo.completed } : todo)
     )
-    axios.put(`/todos.json`, todos).then(() => this.setState({ todos }))
+    axios.put(`/todos.json`, todos).then(() => this.props.updateTodo(todos))
   }
 
   deleteTodo = id => {
-    axios.delete(`/todos/${id}.json`).then(() =>
-      this.setState(prevState => ({
-        todos: prevState.todos.filter(todo => todo.id !== id)
-      }))
-    )
+    axios
+      .delete(`/todos/${id}.json`)
+      .then(() =>
+        this.props.deleteTodo(this.props.todos.filter(todo => todo.id !== id))
+      )
   }
 
   onDragEnd = result => {
@@ -65,19 +66,19 @@ class TodoCard extends Component {
       return
     }
     const todos = reorder(
-      this.state.todos,
+      this.props.todos,
       result.source.index,
       result.destination.index
     )
-    axios.put('/todos.json', todos).then(() => this.setState({ todos }))
+    axios.put('/todos.json', todos).then(() => this.props.updateTodo(todos))
   }
 
   componentDidMount () {
     axios.get('/todos.json').then(response => {
-      console.log(response)
-      this.setState(prevState => ({
-        todos: [...prevState.todos, ...response]
-      }))
+      // this.setState(prevState => ({
+      //   todos: [...prevState.todos, ...response]
+      // }))
+      this.props.getTodos(response)
     })
   }
 
@@ -90,7 +91,7 @@ class TodoCard extends Component {
           keyPressed={this.keyPressed}
         />
         <TodoList
-          todos={this.state.todos}
+          todos={this.props.todos}
           toggleTodo={this.toggleTodo}
           deleteTodo={this.deleteTodo}
           onDragEnd={this.onDragEnd}
@@ -100,4 +101,16 @@ class TodoCard extends Component {
   }
 }
 
-export default TodoCard
+const mapStateToProps = state => ({
+  todos: state.todos
+  // todos: state.todo.todos  For use when combining reducers
+})
+
+const mapDispatchToProps = dispatch => ({
+  getTodos: todos => dispatch({ type: actionTypes.GET_TODOS, todos }),
+  createTodo: todos => dispatch({ type: actionTypes.CREATE_TODO, todos }),
+  updateTodo: todos => dispatch({ type: actionTypes.UPDATE_TODO, todos }),
+  deleteTodo: todos => dispatch({ type: actionTypes.DELETE_TODO, todos })
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoCard)
--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import { Provider } from 'react-redux'
+import { createStore, combineReducers } from 'redux'
+import todoReducer from './store/reducers/todoReducer'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+// This is how you would combine reducers
+// const rootReducer = combineReducers({
+//     todo: todoReducer,
+//     input: inputReducer
+// })
+
+// const store = createStore(rootReducer)
+
+const store = createStore(
+  todoReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+ReactDOM.render(
+  <Provider store={store}><App /></Provider>,
+  document.getElementById('root')
+)
